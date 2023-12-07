345b08f9c61ddfb94fefc3a6315aec289e400fe2
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-07 11:40:49 +0200

    creating compute object to cpu service

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,9 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts from power service');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
