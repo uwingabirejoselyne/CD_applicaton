372dbe1c88220bd1ed464a7465585459542091d3
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-07 11:32:37 +0200

    Sharing service  between power module and cpu module

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PowerService {}
+export class PowerService {
+  supplyPower(watts: number) {
+    console.log(`it will be a ${watts}number`);
+  }
+}
